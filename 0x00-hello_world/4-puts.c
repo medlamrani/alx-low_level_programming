ef0999dd4acf3c4326fd7bf3ac55f2a99d20cae3
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-15 16:26:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Print with puts function
+ *
+ * return 0
+ */
+
+
 int main(void) {
 
-	const char *message = "Programming is like building a multilingual puzzle\n"; 
+	const char *message = "\"Programming is like building a multilingual puzzle"; 
 
 	puts(message);
 
