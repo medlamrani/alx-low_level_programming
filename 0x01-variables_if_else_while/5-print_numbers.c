ebd0ea0f3a936d6038f9f7494874fa0706e7828d
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-16 15:21:28 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints number base of 10 starting from 0
+ *
+ * Return : Always return 0
+*/
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf(number);
+		number++;
+	}
+	printf('\n');
+
+	retunr (0);
+}
