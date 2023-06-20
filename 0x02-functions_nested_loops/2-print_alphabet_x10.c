ba6684601a7bdf6de1200b60ee48e09c0cd4391d
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-20 16:01:08 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include"main.h"
 /**
- *
+ * print_alphabet - use _putchar function 
 */
 void print_alphabet_x10(void)
 {
 	int much = 0;
 	int let;
 
-	while (much <= 10)
+	while (much < 10)
 	{
 		for (let = 'a'; let <= 'z'; let++)
 		{
