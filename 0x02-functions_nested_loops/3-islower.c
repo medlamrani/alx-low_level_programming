bf8b2d5c17ad7dca19210a0469e54cc0bab6a009
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-20 16:47:00 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _islower - function to check is character is lowercase
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}else
+	{
+		return (0);
+	}
+}
+
