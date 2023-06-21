f57839381f60206e2acafaf5be5dfc0e5c28dc4f
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-20 17:32:30 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_sign - print the sign of a number
+ * @n: take input from other function
+ * return: 1 if n is greater than zero
+ *         0 if n is zero
+ *         -1 if n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(43);
+		return (1);
+	else if (n == 0)
+		_putchar(48);
+		return (0);
+	else
+		_putchar(45);
+		return (-1);
+}
