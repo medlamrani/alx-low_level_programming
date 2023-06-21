f734409caad2368ad250a91eaa8308c148dd871e
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-21 05:43:19 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * print_to_98 - print n to 98
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,6 @@ int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
