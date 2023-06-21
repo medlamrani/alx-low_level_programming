306c897a0a0ad1e508b585b21cf84ea77b4f2787
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-21 05:26:50 -0700

    task10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,16 @@
 #include"main.h"
 /**
  * add - adds two integers and return result
+ * @a: first parameter
+ * @b: second parameter
+ * Return: return the result of a + b
 */
 
 int add(int a, int b)
 {
 	int result;
+
 	result = a + b;
 
-	return(result);
+	return (result);
 }
