eb4e7bc53913fd5df9fd1e30ba92045169584864
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-22 08:22:02 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_line - draws a straight line in terminal
+ * @n: number of time
+*/
+
+void print_line(int n)
+{
+	int start;
+
+	for (start = 0; start < n; start++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('-');
+	}
+	_putchar('\n');
+}
