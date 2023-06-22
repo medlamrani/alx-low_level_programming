574999dfe9fec1365c53d79bf4e001b86591c988
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-22 07:20:06 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num + '0');
+			putchar(num + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
