e843845637d25d6dffa56c406dffb356f388416e
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-24 04:24:28 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,12 +9,14 @@ void print_diagonal(int n)
 	int line, space;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (line = 0; line <= n; line++)
+		for (line = 1; line <= n; line++)
 		{
-			for (space = 0; space <= line; space++)
+			for (space = 1; space <= line; space++)
 			{
 				if (line == space)
 				{
