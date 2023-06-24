3a1ff9ad24457491835cb9f45b3eaa7df316d4eb
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-24 06:01:13 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include"main.h"
+/**
+ * main - print number from 0 to 100 except when the num is div by 3 print Fizz
+ * or 5 print Buzz or both print FizzBuzz
+ *
+ * Return: Always return 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; ++num)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+
+		if (num == 100)
+		{
+			printf("\n");
+		}
+	}
+
+	return (0);
+}
