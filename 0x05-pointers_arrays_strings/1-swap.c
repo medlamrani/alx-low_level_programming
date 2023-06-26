5f8e767f2e957aaf61567044333ba0882318ff72
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-26 12:08:23 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * swap_int - swap the values of two integers
+ * 		using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = temp;
+}
