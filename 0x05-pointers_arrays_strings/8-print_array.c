f514227512702dae481ed9bfff1b58903604fd2b
Mohamed Lamrani <med.lamrani93@gmail.com>
2023-06-27 11:49:46 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,13 @@ void print_array(int *a, int n)
 	for (inc = 0; inc < n; inc++)
 	{
 		if (inc != n - 1)
+		{
 			printf("%d, ", a[inc]);
+		}
 		else
+		{
 			printf("%d", a[inc]);
+		}
 	}
 	_putchar(10);
 }
